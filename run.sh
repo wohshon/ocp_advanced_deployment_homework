582a4fa050b9e899be3e543b26154c5e13d95384
root <root@bastion.bd03.example.opentlc.com>
2018-08-22 14:36:49 +0000

    cleaned up formatting

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo '*******************SET GUID********************'
 export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
 echo 'GUID ==> '$GUID
 echo '*******************RUN PLAYBOOK****************'
-sudo ansible-playbook -v -f 20 homework.yaml --extra-vars GUID=$GUID
+ansible-playbook -v -f 20 homework.yaml --extra-vars GUID=$GUID
