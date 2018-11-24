bc58e92e41154772ee864d0858e653f0cede1d48
root <root@bastion.fc39.example.opentlc.com>
2018-11-24 13:54:04 +0000

    run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo '*******************SET GUID********************'
 export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
 echo 'GUID ==> '$GUID
 echo '*******************RUN PLAYBOOK****************'
-ansible-playbook -v -f 20 homework.yaml --extra-vars GUID=$GUID
+ansible-playbook -v -f 20 homework.yaml --extra-vars GUID=$GUID --extra-vars oreg_user=$OREG_USER --extra-vars oreg_password=$OREG_PASSWORD
